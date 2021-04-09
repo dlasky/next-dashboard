7b3ed97395c4f272a3f879f71c68e48d20c9bce6
Dan <github@aerolith.net>
2021-04-08 21:06:31 -0400

    timer

--- hooks/useRefresh.ts
+++ hooks/useRefresh.ts
@@ -3,8 +3,13 @@ import { useEffect, useState } from "react";
 const useRefresh = (ms: number = 1000) => {
   const [time, setTime] = useState(Date.now());
   useEffect(() => {
-    setTimeout(() => setTime(Date.now()), ms);
-  }, []);
+    let interval = setInterval(() => {
+      setTime(Date.now());
+    }, ms);
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [ms]);
   //TODO: consider stablizing the object
   return { time };
 };
