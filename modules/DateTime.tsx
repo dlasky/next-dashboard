fc583980cfe30b4fe508dbd5b9f2c54dc3c8557c
Dan <github@aerolith.net>
2021-04-08 21:07:24 -0400

    big clock module

---
+++ modules/DateTime.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { DateTime } from "luxon";
+import styled from "@emotion/styled";
+import { Module, Sup, Div } from "../components/sharedStyles";
+import useRefresh from "../hooks/useRefresh";
+
+const FixedSup = styled(Sup)`
+  display: inline-block;
+  min-width: 37px;
+`;
+
+const renderTimePart = ({ type, value }) => {
+  switch (type) {
+    default:
+      return value;
+    case "hour":
+      return `${value}:`;
+    case "second":
+      return (
+        <FixedSup dimmed key="sec">
+          {value}
+        </FixedSup>
+      );
+    case "literal":
+      return "";
+    case "dayPeriod":
+      return value.toLowerCase();
+  }
+};
+
+const DT = () => {
+  const { time } = useRefresh(1000);
+  const local = DateTime.fromMillis(time).setZone("America/New_York");
+  return (
+    <Module>
+      <Div normal medium>
+        {local.toFormat("DDDD")}
+      </Div>
+      <Div large bright>
+        {local.toLocaleParts(DateTime.TIME_WITH_SECONDS).map(renderTimePart)}
+      </Div>
+    </Module>
+  );
+};
+
+export default DT;
