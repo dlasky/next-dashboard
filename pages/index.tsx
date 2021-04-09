8ccfd9ed8056ba1456577886a4a2bdc2a3d2c7f5
Dan <github@aerolith.net>
2021-04-08 21:07:32 -0400

    index + pkg updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import { Grid, Two, Four } from "../components/Grid";
 import styled from "@emotion/styled";
 import WorldClock from "../modules/WorldClock";
+import DateTime from "../modules/DateTime";
+import Holidays from "../modules/Holidays";
+import Pollen from "../modules/Pollen";
 
 const Content = styled("div")`
   font-family: "Rajdhani", sans-serif;
@@ -13,9 +16,9 @@ const Index = () => (
     {/* <Status /> */}
     <Grid>
       <Two>
-        {/* <DateTime /> */}
-        {/* <Holidays maxItems={5} /> */}
-        {/* <Pollen /> */}
+        <DateTime />
+        <Holidays maxItems={5} />
+        <Pollen />
         {/* <Hue /> */}
       </Two>
       <Two>{/*<R>
